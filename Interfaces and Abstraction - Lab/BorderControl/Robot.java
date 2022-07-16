6c0cdd2e8d7c4a62f77bcacbde1075b1f12f2595
Lubomir Pandurski <108551504+LubomirPandurski@users.noreply.github.com>
2022-07-16 22:54:34 +0300

    Add files via upload

---
+++ Interfaces and Abstraction - Lab/BorderControl/Robot.java
@@ -0,0 +1,15 @@
+package BorderControl;
+
+public class Robot extends IdentifiableImpl{
+
+    private String model;
+
+    public Robot(String id, String model) {
+        super(id);
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
