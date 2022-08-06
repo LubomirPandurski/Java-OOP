daf5dc54679c9e73ec0180938b6adcd1c70d185a
Lubomir Pandurski <108551504+LubomirPandurski@users.noreply.github.com>
2022-08-06 11:36:43 +0300

    Add files via upload

---
+++ Unit Testing - Lab/HeroTest1.java
@@ -0,0 +1,14 @@
+package rpg_lab;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HeroTest1 {
+
+    @Test
+    public void test_HeroGainsXP_When_KillTarget_Mocking_Example(){
+
+    }
+
+}
